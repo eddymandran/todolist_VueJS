2ebf8eb0bebaa35fb871fa6d67a269b2f9137b7b
eddymandran <eddy.mandran@gmail.com>
2021-02-02 16:59:29 +0100

    create component todos.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
+<div>
+  <todos></todos>
+</div>
+<!-- </div>
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
-  </div>
+  </div> -->
 </template>
 
 <script>
+import Todos from './components/Todos'
 export default {
-  name: 'App'
+  components: {
+    Todos
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
