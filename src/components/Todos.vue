d52f5c8ff118fdca9f97dfdca5eb4daf3df432fd
eddymandran <eddy.mandran@gmail.com>
2021-02-11 16:48:22 +0100

    can add task and display how much task remaining

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,21 +2,49 @@
     <section class="todoapp">
         <header class="header">
             <h1>Todos</h1>
-            <input type="text" class="new-todo" placeholder="Ajouter une tache"/>
+            <input type="text" class="new-todo" placeholder="Ajouter une tache" v-model="newTodo" @keyup.enter="addTodo"/>
         </header>
         <div class="main">
             <ul class="todo-list">
-                <li class="todo">
+                <li class="todo" v-for="todo in todos" :class="{completed: todo.completed}">
                     <div class="view">
-                        <label> Nom de notre tache</label>
+                      <input type="checkbox" v-model="todo.completed" class="toggle"/>
+                        <label> {{todo.name}}</label>
                     </div>
                 </li>
             </ul>
         </div>
+        <footer class="footer">
+          <span class="todo-count"><strong>{{remaining}}</strong> Tâche(s) a faire</span>
+        </footer>
     </section>
 </template>
 <script>
 export default {
+  data () {
+    return {
+      todos: [{
+        name: 'Tache de test',
+        completed: false
+      }],
+      newTodo: ''
+    }
+  },
+  methods: {
+    addTodo () {
+      this.todos.push({
+        completed: false,
+
+        name: this.newTodo
+      })
+      this.newTodo = ''
+    }
+  },
+  computed: {
+    remaining () {
+      return this.todos.filter(todo => !todo.completed).length
+    }
+  }
 }
 </script>
 <style src="./todos.css"></style>
